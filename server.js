2b27e58cbe7fb9f738ebacc745cadc1f00edb197
benjaminsinaiko <ben.jamin@outlook.com>
2019-03-13 15:20:50 -0500

    Set up server for static files and  production

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const Parse = require('parse/node')
 
@@ -27,5 +28,21 @@ Parse.serverURL = 'https://parseapi.back4app.com/'
 app.use('/', router)
 
 const port = process.env.PORT || 5000
+
+// Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')))
+  })
+}
+
+// Build
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client/public/index.html'))
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
