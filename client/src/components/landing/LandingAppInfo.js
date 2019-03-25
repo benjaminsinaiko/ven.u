48b1d17ba201b4d6cfe848497d00c46310ae332c
benjaminsinaiko <ben.jamin@outlook.com>
2019-03-25 16:28:46 -0500

    Add app store images, update layout

--- client/src/components/landing/LandingAppInfo.js
+++ client/src/components/landing/LandingAppInfo.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import { Grid, Typography, Card, CardMedia } from '@material-ui/core';
+import { Grid, Typography, Card, CardMedia, Button } from '@material-ui/core';
 
 import { withStyles } from '@material-ui/core/styles';
 
 import discoverImage from '../../assets/discoverImage.png';
+import artistImage from '../../assets/artistImage.png';
+import venueImage from '../../assets/venueImage.png';
+import historyImage from '../../assets/historyImage.png';
+import downloadAppStore from '../../assets/downloadAppStore.png';
 
 const styles = theme => ({
   root: {
-    backgroundColor: '#E9E8E8',
-    padding: 20,
+    backgroundColor: '#CFD8DC',
+    paddingTop: 50,
+    paddingBottom: 20,
   },
   grid: {
-    maxWidth: 900,
+    maxWidth: 1200,
     margin: 'auto',
   },
   card: {
-    maxHeight: 600,
-    maxWidth: 337.5,
-    [theme.breakpoints.down('sm')]: {
+    maxHeight: 500,
+    maxWidth: 281.25,
+    [theme.breakpoints.down('md')]: {
       maxHeight: 400,
       maxWidth: 225,
     },
-    margin: 'auto',
+    margin: '20px auto',
   },
   media: {
     height: '100%',
     width: '100%',
+  },
+  appButton: {
+    backgroundImage: `url(${downloadAppStore})`,
+    width: 135,
+    height: 40,
   },
 });
 
@@ -34,48 +44,54 @@ const LandingAppInfo = props => {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h3" gutterBottom>
-        Get the app
+      <Typography variant="h5" gutterBottom>
+        The best companion app for live music fans in Chicago.
       </Typography>
 
-      <Grid container className={classes.grid} spacing={24}>
-        <Grid item sm={6}>
+      <Grid container className={classes.grid} spacing={16}>
+        <Grid item md={3}>
           <Card className={classes.card}>
             <CardMedia
               className={classes.media}
               component="img"
               image={discoverImage}
+              title="Discover"
             />
           </Card>
         </Grid>
-        <Grid item sm={6}>
+        <Grid item md={3}>
           <Card className={classes.card}>
             <CardMedia
               className={classes.media}
               component="img"
-              image={discoverImage}
+              image={artistImage}
+              title="Artist info"
             />
           </Card>
         </Grid>
-        <Grid item sm={6}>
+        <Grid item md={3}>
           <Card className={classes.card}>
             <CardMedia
               className={classes.media}
               component="img"
-              image={discoverImage}
+              image={venueImage}
+              title="Venue info"
             />
           </Card>
         </Grid>
-        <Grid item sm={6}>
+        <Grid item md={3}>
           <Card className={classes.card}>
             <CardMedia
               className={classes.media}
               component="img"
-              image={discoverImage}
+              image={historyImage}
+              title="Show history"
             />
           </Card>
         </Grid>
       </Grid>
+
+      <Button className={classes.appButton} />
     </div>
   );
 };
