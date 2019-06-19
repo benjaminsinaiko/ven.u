fcdf3c2d7bfc5d8fd4b9d30cedeb9f21474b9bb4
benjaminsinaiko <ben.jamin@outlook.com>
2019-06-19 12:03:32 -0500

    Refactor Footer, move styles out

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,82 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
 import { Grid, Typography } from '@material-ui/core';
 
+import useStyles from './styles/FooterStyles';
 import logoName from '../../assets/navLogo.png';
 
-const styles = theme => ({
-  root: {
-    backgroundColor: '#212121',
-    padding: theme.spacing.unit * 3
-  },
-  social: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-end'
-  },
-  socialIcons: {
-    color: 'white',
-    '&:hover': {
-      color: '#E48611'
-    },
-    margin: theme.spacing.unit * 2
-  },
-  text: {
-    color: 'white'
-  },
-  links: {
-    color: 'white',
-    textDecoration: 'none',
-    '&:hover': {
-      color: '#E48611'
-    }
-  }
-});
-
-const Footer = props => {
-  const { classes } = props;
+const Footer = () => {
+  const classes = useStyles();
 
   return (
     <footer className={classes.root}>
-      <Grid container className={classes.text}>
-        <Grid item sm={4} style={{ alignSelf: 'center' }}>
-          <Link to="/" className={classes.links}>
-            <img src={logoName} alt="logo" />
-          </Link>
-        </Grid>
-        <Grid item sm={4} className={classes.social}>
-          <a href="http://www.facebook.com/getvenu">
-            <i className={`fab fa-facebook-f ${classes.socialIcons}`} />
-          </a>
-          <a href="http://www.twitter.com/venuCrew">
-            <i className={`fab fa-twitter ${classes.socialIcons}`} />
-          </a>
-          <a href="http://www.instagram.com/venuCrew">
-            <i className={`fab fa-instagram ${classes.socialIcons}`} />
-          </a>
-          <a href="mailto:hello@getvenu.com">
-            <i className={`fas fa-at ${classes.socialIcons}`} />
-          </a>
-        </Grid>
-        <Grid item sm={4}>
-          <Typography className={classes.text} variant="body2">
-            <Link to="/privacy" className={classes.links}>
-              Privacy Policy
-            </Link>
-          </Typography>
-          <Typography className={classes.text} variant="caption">
-            &copy; {new Date().getFullYear()} Tunimal, LLC
-          </Typography>
-        </Grid>
+      <Link to="/">
+        <img src={logoName} alt="logo" className={classes.logo} />
+      </Link>
+
+      <Grid item sm={4} className={classes.social}>
+        <a href="http://www.facebook.com/getvenu">
+          <i className={`fab fa-facebook-f ${classes.socialIcons}`} />
+        </a>
+        <a href="http://www.twitter.com/venuCrew">
+          <i className={`fab fa-twitter ${classes.socialIcons}`} />
+        </a>
+        <a href="http://www.instagram.com/venuCrew">
+          <i className={`fab fa-instagram ${classes.socialIcons}`} />
+        </a>
+        <a href="mailto:hello@getvenu.com">
+          <i className={`fas fa-at ${classes.socialIcons}`} />
+        </a>
       </Grid>
+
+      <div className={classes.legal}>
+        <Typography variant="body2">
+          <Link to="/privacy" className={classes.privacy}>
+            Privacy Policy
+          </Link>
+        </Typography>
+        <Typography variant="caption">&copy; {new Date().getFullYear()} Tunimal, LLC</Typography>
+      </div>
     </footer>
   );
 };
 
-Footer.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Footer);
+export default Footer;
---
+++ client/src/components/layout/styles/FooterStyles.js
@@ -0,0 +1,42 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    color: 'white',
+    backgroundColor: '#212121',
+    padding: theme.spacing.unit * 3,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column'
+    }
+  },
+  logo: {
+    '&:hover': {
+      filter:
+        'invert(54%) sepia(31%) saturate(3755%) hue-rotate(5deg) brightness(101%) contrast(87%)'
+    }
+  },
+  social: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  socialIcons: {
+    color: 'white',
+    fontSize: '1.3rem',
+    margin: theme.spacing.unit * 2,
+    '&:hover': {
+      color: '#E48611'
+    }
+  },
+  privacy: {
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#E48611'
+    }
+  }
+}));
+
+export default useStyles;
