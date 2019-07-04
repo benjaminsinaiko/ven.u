a05168fd5b5b612e17272ad1e9fb246d9acdb596
benjaminsinaiko <ben.jamin@outlook.com>
2019-07-04 01:23:41 -0500

    Add private route, rearrange app and landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+
+import NavBar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Privacy from './components/privacy/Privacy';
+import LandingPage from './components/landing/LandingPage';
+import AddEventsPage from './components/addEvents/AddEventsPage';
 
 import AppProviders from './contexts/AppProviders';
-import AppLanding from './AppLanding';
 
 function App() {
   return (
     <AppProviders>
-      <AppLanding />
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <PrivateRoute exact path="/add-events" component={AddEventsPage} />
+        <Route path="/privacy" component={Privacy} />
+      </Switch>
+      <Footer />
     </AppProviders>
   );
 }
--- client/src/components/addEvents/styles/DateTimePickerStyles.js
+++ client/src/components/addEvents/styles/DateTimePickerStyles.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    width: '50%'
   },
   textField: {
     marginLeft: theme.spacing(1),
